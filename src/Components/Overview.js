04ddcc0ce3e5723b174c265dfdde4a77c908a340
lukkulig <lukasz.kulig95@gmail.com>
2019-06-25 18:37:02 +0200

    Added greeting handling as example

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -3,15 +3,38 @@ import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles/index';
 import Typography from '@material-ui/core/Typography/index';
 import styles from "./Overview.styles";
+import api from "../api";
 
 class Overview extends React.Component {
+
+    state = {
+        greetings: "",
+    };
+
+    fetchAndSetGreetings() {
+        document.body.style.cursor = 'wait';
+        api.fetchString(
+            api.endpoints.getGreeting(),
+            (response) => {
+                this.setState({greetings: response});
+            });
+    }
+
+    componentDidMount() {
+        this.fetchAndSetGreetings();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+    }
+
     render() {
         const {classes} = this.props;
         return (
             <div className={classes.root}>
                 <div className={classes.content}>
                     <Typography variant="h4" gutterBottom>
-                        Overview
+                        <p>Overview</p>
+                        <p>{this.state.greetings}</p>
                     </Typography>
                 </div>
             </div>
